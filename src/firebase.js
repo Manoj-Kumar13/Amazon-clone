71adc60ee289a51065680e6afac66d4593ead17c
Manoj <69189606+Manoj-Kumar13@users.noreply.github.com>
2021-11-20 14:36:23 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB5V6KPBjT2KCdpf8jv1XmdchO-GbZU6do",
-  authDomain: "challenge-af234.firebaseapp.com",
-  projectId: "challenge-af234",
-  storageBucket: "challenge-af234.appspot.com",
-  messagingSenderId: "347771757853",
-  appId: "1:347771757853:web:59fda9020197ead8f26df4",
-  measurementId: "G-0Q9GDNKCK7",
+ // FIREBASE CONFIG
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
